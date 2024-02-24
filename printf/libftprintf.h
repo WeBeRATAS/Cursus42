2029b135a23f83ab6f8871b687902369dff47274
Rubén BM <150420895+WeBeRATAS@users.noreply.github.com>
2024-02-24 12:30:32 +0100

    Add files via upload

---
+++ printf/libftprintf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbuitrag <rbuitrag@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 11:29:59 by rbuitrag          #+#    #+#             */
+/*   Updated: 2024/02/24 11:33:48 by rbuitrag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdarg.h>
+# include <stdint.h>
+
+
+
+
+#endif
